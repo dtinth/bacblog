8ad6a6d5a37c2e7ccc43bfbed911744dbdfab87e
dtinth on MBP M1 <dtinth@spacet.me>
2024-11-08 20:57:24 +0700

    initial commit

---
+++ _scripts/write.mjs
@@ -0,0 +1,95 @@
+import fs from "fs";
+import OpenAI from "openai";
+const openai = new OpenAI();
+
+async function generateIntro(title) {
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text:
+              'Write an introduction paragraph of an article. The title is "' +
+              title +
+              '". DO NOT use aposthrophes (\') or quotes (") or slashes or hyphens in the article. No new lines.',
+          },
+        ],
+      },
+    ],
+    temperature: 0,
+    max_tokens: 2048,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    response_format: {
+      type: "json_schema",
+      json_schema: {
+        name: "intro_schema",
+        strict: true,
+        schema: {
+          type: "object",
+          properties: {
+            intro: {
+              type: "string",
+              description: "An article introduction string.",
+            },
+          },
+          required: ["intro"],
+          additionalProperties: false,
+        },
+      },
+    },
+  });
+  return JSON.parse(response.choices[0].message.content)
+    .intro.trim()
+    .replace(/-/g, " ")
+    .trim();
+}
+
+import crypto from "crypto";
+const lines = fs
+  .readFileSync("scripts/list.tsv", "utf8")
+  .split("\n")
+  .map((x) => x.trim())
+  .filter((x) => x);
+let ok = true;
+for (const title of lines) {
+  const hash = crypto
+    .createHash("sha256")
+    .update(title)
+    .digest("hex")
+    .slice(0, 32);
+  const file = `_posts/2023-11-20-${hash}.markdown`;
+  if (fs.existsSync(file)) {
+    continue;
+  }
+  console.log(file, title);
+  const intro = await generateIntro(title);
+  const wordCount1 = intro.trim().split(" ").length;
+  const wordCount2 = [...intro.matchAll(/\w+/g)].length;
+  console.log(intro);
+  if (wordCount1 !== wordCount2) {
+    ok = false;
+    continue;
+  }
+  fs.writeFileSync(
+    file,
+    `---
+layout: post
+title: "${title}"
+date: 2023-11-20
+---
+
+${intro}
+`
+  );
+  console.log();
+}
+
+if (!ok) {
+  console.error("Word count mismatch");
+  process.exit(1);
+}
